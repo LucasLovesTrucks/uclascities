4f4b6f2dd54c2eebdb688fc7e58c3d3354ec6994
LucasLovesTrucks <lucaslovestrucks@outlook.com>
2026-07-31 22:25:44 +1000

    mod files

---
+++ src/main/java/net/uclas/uclascities/UclasCityModClient.java
@@ -0,0 +1,10 @@
+package net.uclas.uclascities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UclasCityModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/uclas/uclascities/UclasCityModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.uclas.uclascities;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UclasCityModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
